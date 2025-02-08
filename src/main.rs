1ef13e404e64aa0d0d4edef51181e9f46ece3544
krishna2803 <kpandey81930@gmail.com>
2025-02-09 01:19:25 +0530

    feat: pretty logger

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use std::env;
 
-use bot_man::tgbot;
 use bot_man::agent;
+use bot_man::tgbot;
+
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "trace");
+    }
+
+    pretty_env_logger::init();
+
+    env::remove_var("RUST_APP_LOG");
+    
     let agent_url = env::var("AGENT_URL").expect("AGENT_URL must be set");
     let autonome_user = env::var("AUTONOME_USER").expect("AUTONOME_USER must be set");
     let autonome_password = env::var("AUTONOME_PASSWORD").expect("AUTONOME_PASSWORD must be set");
 
     let message = "Hello";
 
+    info!("connecting to agent...");
     let response = agent::get_agent_response(
         &agent_url,
         &autonome_user,
@@ -20,7 +33,9 @@ async fn main() {
         message,
     ).await.expect("Failed to get response from agent");
 
-    println!("{}", response);
+    info!("response from agent: {}", response);
+
+    info!("starting the bot...");
 
     tgbot::deploy().await;
 }
