2f51232920d54f44ee73b134f084acd411bf0029
krishna2803 <kpandey81930@gmail.com>
2025-02-09 03:44:41 +0530

    refactor: agent connection and response sending to telegram

--- src/main.rs
+++ src/main.rs
@@ -17,28 +17,6 @@ async fn main() {
 
     pretty_env_logger::init();
 
-    let agent_url = env::var("AGENT_URL").expect("AGENT_URL must be set");
-    let autonome_user = env::var("AUTONOME_USER").expect("AUTONOME_USER must be set");
-    let autonome_password = env::var("AUTONOME_PASSWORD").expect("AUTONOME_PASSWORD must be set");
-
-    let message = "how are you doing?";
-
-    // let response =
-    //     agent::get_agent_response(&agent_url, &autonome_user, &autonome_password, message)
-    //         .await
-    //         .expect("Failed to get response from agent");
-
-    info!("connecting to agent...");
-
-    let response = agent::get_agent_response(
-        &agent_url,
-        &autonome_user,
-        &autonome_password,
-        message,
-    ).await.expect("Failed to get response from agent");
-
-    info!("response from agent: {}", response);
-
     info!("starting the bot...");
 
     tgbot::deploy().await;
--- src/tgbot/mod.rs
+++ src/tgbot/mod.rs
@@ -8,36 +8,30 @@ use teloxide::{
 
 use crate::agent;
 
+
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 
 async fn hello_world(bot: Bot, message: Message) -> HandlerResult {
-    info!("Received a message: {:?}", message.text());
     if let Some(msg) = message.text() {
-
         info!("sending message=\"{}\" to agent...", msg);
 
         let agent_url = env::var("AGENT_URL").expect("AGENT_URL must be set");
         let autonome_user = env::var("AUTONOME_USER").expect("AUTONOME_USER must be set");
-        let autonome_password =
-            env::var("AUTONOME_PASSWORD").expect("AUTONOME_PASSWORD must be set");
+        let autonome_password = env::var("AUTONOME_PASSWORD").expect("AUTONOME_PASSWORD must be set");
 
-        info!("connecting to agent...");
+        let agent= agent::Agent::new(agent_url, autonome_user, autonome_password);
 
-        let res =
-            agent::get_agent_response(&agent_url, &autonome_user, &autonome_password, msg)
-                .await
-                .expect("Failed to get response from agent");
+        let res = agent.get_response(&msg).await.unwrap();
 
         info!("response from agent: {}", res);
 
-        bot.send_message(message.chat.id, res).await?;
+        bot.send_message(message.chat.id, res).send().await?;
     }
     Ok(())
 }
 
 fn handler_tree() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>> {
-    // A simple handler. But you need to make it into a separate thing!
-    dptree::entry().branch(Update::filter_message().endpoint(hello_world))
+    Update::filter_message().endpoint(hello_world)
 }
 
 pub async fn deploy() {
@@ -59,37 +53,22 @@ mod tests {
 
     #[tokio::test]
     async fn test_hello_world() {
-        // This is a message builder. You can check the docs for more info about mocked types
         let mock_message = MockMessageText::new().text("Hi!");
-        // This creates a fake bot that will send the mock_message after we dispatch it as if it was sent by the user
-        // If you wanted, you could've made vec![MockMessageText::new().text("Hi!"), MockMessageText::new().text("Hello!")],
-        // and both updates would've been sent one after the other. You also can make a MockMessagePhoto, MockMessageDocument, etc
         let bot = MockBot::new(mock_message, handler_tree());
-        // This will dispatch the update
         bot.dispatch().await;
 
-        // We can now check the sent messages
-        let responses = bot.get_responses(); // This returns a struct that has all of the recieved
-                                             // updates and requests. You can treat that function like a variable, because it basically is.
+        let responses = bot.get_responses();
         let message = responses
-            .sent_messages // This is a list of all sent messages. Be warned, editing or deleting
-            // messages do not affect this list!
+            .sent_messages
             .last()
             .expect("No sent messages were detected!");
         assert_eq!(message.text(), Some("Hello World!"));
 
-        // There is also a more specialized field, sent_messages_text:
         let message_text = responses
-            .sent_messages_text // This has a list request bodies and sent messages of only text messages, no photo, audio, etc.
-            // messages
+            .sent_messages_text
             .last()
             .expect("No sent messages were detected!");
         assert_eq!(message_text.message.text(), Some("Hello World!"));
-        // The 'bot_request' field is what the bot sent to the fake server. It has some fields that
-        // can't be accessed by looking only at the resulted message. For example, drop-down style keyboards can't
-        // be seen in the regular message, like the parse_mode.
         assert_eq!(message_text.bot_request.parse_mode, None);
-        // Also, it is highly discouraged to use the raw bot fields like bot.updates and bot.bot,
-        // abstractions exist for a reason!!! Do not use them unless you know what you are doing!
     }
 }
