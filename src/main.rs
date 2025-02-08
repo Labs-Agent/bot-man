b6997f4d8f55bf61d681578bf5884bed44c240ac
krishna2803 <kpandey81930@gmail.com>
2025-02-08 23:07:37 +0530

    add: initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use teloxide::prelude::*;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Load environment variables from .env file.
+    // Fails if .env file not found, not readable or invalid.
+    dotenvy::dotenv()?;
+
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
+
+    Ok(())
+}
